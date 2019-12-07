15eea50030b14fa399ddd15bf8d2b2dbece68c16
Abhinau Kumar <ab.kumr98@gmail.com>
2019-12-07 15:43:50 -0600

    Updated to work on TACC

--- analyze_scale.sh
+++ analyze_scale.sh
@@ -5,15 +5,15 @@ resolution=$2
 
 echo "Starting resolution ${resolution}"
 
-for file in "${directory}/*.mp4"
+for file in ${directory}/*.mp4
 do
     filename="${file##*/}"
     filename="${filename%.*}"
 
-    ffmpeg -hide_banner -loglevel panic -i "${directory}/${filename}.mp4" -vf scale=$resolution -sws_flags lanczos -y "temp/${resolution##*:}_scaled_video.mp4"
-    ffmpeg -hide_banner -loglevel panic -i "temp/${resolution##*:}_scaled_video.mp4" -vf scale=1920:1080 -sws_flags lanczos -y "temp/${resolution##*:}_upscaled_video.mp4"
+    ffmpeg -i "${directory}/${filename}.mp4" -vf scale=$resolution -sws_flags lanczos -y "temp/${resolution##*:}_scaled_video.mp4"
+    ffmpeg -i "temp/${resolution##*:}_scaled_video.mp4" -vf scale=1920:1080 -sws_flags lanczos -y "temp/${resolution##*:}_upscaled_video.mp4"
 
-    ffmpeg -hide_banner -loglevel panic -i "temp/${resolution##*:}_upscaled_video.mp4" -i "${directory}/${filename}.mp4" -lavfi ssim="ssims/${filename}_${resolution##*:}_ref_ssim.log" -f null -
+    ffmpeg -i "temp/${resolution##*:}_upscaled_video.mp4" -i "${directory}/${filename}.mp4" -lavfi ssim="ssims/${filename}_${resolution##*:}_ref_ssim.log" -f null -
 
     ./analyze_video.sh $directory $filename $resolution
 
--- analyze_video.sh
+++ analyze_video.sh
@@ -10,11 +10,11 @@ for qp in $qps
 do
 
 echo "${file}, ${resolution}, ${qp}"
-ffmpeg -hide_banner -loglevel panic -i "temp/${resolution##*:}_scaled_video.mp4" -vcodec libx264 -q $qp -y "temp/${resolution##*:}_comp_video.mp4"
-ffmpeg -hide_banner -loglevel panic -i "temp/${resolution##*:}_comp_video.mp4" -vf scale=1920:1080 -sws_flags lanczos -y "temp/${resolution##*:}_upcomp_video.mp4"
+ffmpeg -i "temp/${resolution##*:}_scaled_video.mp4" -vcodec libx264 -crf $qp -y "temp/${resolution##*:}_comp_video.mp4"
+ffmpeg -i "temp/${resolution##*:}_comp_video.mp4" -vf scale=1920:1080 -sws_flags lanczos -y "temp/${resolution##*:}_upcomp_video.mp4"
 
-ffmpeg -hide_banner -loglevel panic -i "temp/${resolution##*:}_comp_video.mp4" -i "temp/${resolution##*:}_scaled_video.mp4" -lavfi ssim="ssims/${file}_${resolution##*:}_${qp}_comp_ssim.log" -f null -
-ffmpeg -hide_banner -loglevel panic -i "temp/${resolution##*:}_upcomp_video.mp4" -i "${directory}/${file}.mp4" -lavfi ssim="ssims/${file}_${resolution##*:}_${qp}_true_ssim.log" -f null -
+ffmpeg -i "temp/${resolution##*:}_comp_video.mp4" -i "temp/${resolution##*:}_scaled_video.mp4" -lavfi ssim="ssims/${file}_${resolution##*:}_${qp}_comp_ssim.log" -f null -
+ffmpeg -i "temp/${resolution##*:}_upcomp_video.mp4" -i "${directory}/${file}.mp4" -lavfi ssim="ssims/${file}_${resolution##*:}_${qp}_true_ssim.log" -f null -
 
 done;
 
