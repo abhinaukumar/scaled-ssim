50de474f44f2854f6a07c6e6bf994df1b610b402
Abhinau Kumar <ab.kumr98@gmail.com>
2020-01-04 18:18:19 -0600

    Updated to include new implementation and database

--- create_plots.py
+++ create_plots.py
@@ -14,22 +14,22 @@ models_dict = {'linear_svm_2': 'Linear SVM (2)',
 
 n_models = len(models_dict.keys())
 
-# Plot variation with training size
-n_trains = np.arange(2500, 26000, 2500)
-fig, axs = plt.subplots(1, 2)
-for i, model in enumerate(models_dict.keys()):
-    f = loadmat(os.path.join('results', 'phase2', model + '_train_size_analysis.mat'))
+# # Plot variation with training size
+# n_trains = np.arange(2500, 26000, 2500)
+# fig, axs = plt.subplots(1, 2)
+# for i, model in enumerate(models_dict.keys()):
+#     f = loadmat(os.path.join('results', 'phase2', model + '_train_size_analysis.mat'))
 
-    axs[0].plot(n_trains, f[model + '_train_pcc'].squeeze(), linestyle='-', color=colors[i], label=models_dict[model] + ' - Train PCC')
-    axs[0].plot(n_trains, f[model + '_test_pcc'].squeeze(), linestyle='--', color=colors[i], label=models_dict[model] + ' - Test PCC')
+#     axs[0].plot(n_trains, f[model + '_train_pcc'].squeeze(), linestyle='-', color=colors[i], label=models_dict[model] + ' - Train PCC')
+#     axs[0].plot(n_trains, f[model + '_test_pcc'].squeeze(), linestyle='--', color=colors[i], label=models_dict[model] + ' - Test PCC')
 
-    axs[1].plot(n_trains, f[model + '_train_srocc'].squeeze(), linestyle='-', color=colors[i], label=models_dict[model] + ' - Train SROCC')
-    axs[1].plot(n_trains, f[model + '_test_srocc'].squeeze(), linestyle='--', color=colors[i], label=models_dict[model] + ' - Test SROCC')
+#     axs[1].plot(n_trains, f[model + '_train_srocc'].squeeze(), linestyle='-', color=colors[i], label=models_dict[model] + ' - Train SROCC')
+#     axs[1].plot(n_trains, f[model + '_test_srocc'].squeeze(), linestyle='--', color=colors[i], label=models_dict[model] + ' - Test SROCC')
 
-axs[0].set_title('PCC')
-axs[0].legend()
-axs[1].set_title('SROCC')
-axs[1].legend()
+# axs[0].set_title('PCC')
+# axs[0].legend()
+# axs[1].set_title('SROCC')
+# axs[1].legend()
 
 # Plot variation with scale and QP
 scales = np.array([144, 240, 360, 480, 540, 720])
@@ -39,33 +39,61 @@ n_qps = len(qps)
 
 plt.figure()
 for i, model in enumerate(models_dict.keys()):
-    f = loadmat(os.path.join('results', 'phase2', model + '_scale_qp_analysis.mat'))
+    f = loadmat(os.path.join('results', 'phase3', model + '_scale_qp_analysis.mat'))
 
     plt.subplot(2, n_models, i + 1)
-    plt.imshow(f[model + '_train_pcc'], vmin=0.9, vmax=1, aspect='equal')
-    plt.title(models_dict[model] + 'Train PCC')
+    plt.imshow(f[model + '_train_pcc'], vmin=0.8, vmax=1, aspect='equal')
+    plt.title(models_dict[model] + ' Train PCC')
     plt.xticks(np.arange(n_qps), qps)
     plt.yticks(np.arange(n_scales), scales)
+    plt.colorbar(orientation='horizontal')
+    plt.subplots_adjust(left=0.05, right=0.95, top=0.9, bottom=0.1, wspace=0.3)
 
     plt.subplot(2, n_models, i + n_models + 1)
-    plt.imshow(f[model + '_test_pcc'], vmin=0.9, vmax=1, aspect='equal')
-    plt.title(models_dict[model] + 'Test PCC')
+    plt.imshow(f[model + '_test_pcc'], vmin=0.8, vmax=1, aspect='equal')
+    plt.title(models_dict[model] + ' Test PCC')
     plt.xticks(np.arange(n_qps), qps)
     plt.yticks(np.arange(n_scales), scales)
+    plt.colorbar(orientation='horizontal')
+    plt.subplots_adjust(left=0.05, right=0.95, top=0.9, bottom=0.1, wspace=0.3)
 
 plt.figure()
 for i, model in enumerate(models_dict.keys()):
-    f = loadmat(os.path.join('results', 'phase2', model + '_scale_qp_analysis.mat'))
+    f = loadmat(os.path.join('results', 'phase3', model + '_scale_qp_analysis.mat'))
 
     plt.subplot(2, n_models, i + 1)
-    plt.imshow(f[model + '_train_srocc'], vmin=0.9, vmax=1, aspect='equal')
-    plt.title(models_dict[model] + 'Train SROCC')
+    plt.imshow(f[model + '_train_srocc'], vmin=0.8, vmax=1, aspect='equal')
+    plt.title(models_dict[model] + ' Train SROCC')
     plt.xticks(np.arange(n_qps), qps)
     plt.yticks(np.arange(n_scales), scales)
+    plt.colorbar(orientation='horizontal')
+    plt.subplots_adjust(left=0.05, right=0.95, top=0.9, bottom=0.1, wspace=0.3)
 
     plt.subplot(2, n_models, i + n_models + 1)
-    plt.imshow(f[model + '_test_srocc'], vmin=0.9, vmax=1, aspect='equal')
-    plt.title(models_dict[model] + 'Test SROCC')
+    plt.imshow(f[model + '_test_srocc'], vmin=0.8, vmax=1, aspect='equal')
+    plt.title(models_dict[model] + ' Test SROCC')
 
     plt.xticks(np.arange(n_qps), qps)
     plt.yticks(np.arange(n_scales), scales)
+    plt.colorbar(orientation='horizontal')
+    plt.subplots_adjust(left=0.05, right=0.95, top=0.9, bottom=0.1, wspace=0.3)
+
+f = loadmat(os.path.join('results', 'phase2', 'prod_scale_qp_analysis.mat'))
+
+plt.figure()
+
+plt.subplot(1, 2, 1)
+plt.imshow(f['prod_pcc'], vmin=0.8, vmax=1, aspect='equal')
+plt.title('Product - PCC')
+plt.xticks(np.arange(n_qps), qps)
+plt.yticks(np.arange(n_scales), scales)
+plt.colorbar(orientation='horizontal')
+plt.subplots_adjust(left=0.05, right=0.95, wspace=0.3)
+
+plt.subplot(1, 2, 2)
+plt.imshow(f['prod_srocc'], vmin=0.8, vmax=1, aspect='equal')
+plt.title('Product - SROCC')
+plt.xticks(np.arange(n_qps), qps)
+plt.yticks(np.arange(n_scales), scales)
+plt.colorbar(orientation='horizontal')
+plt.subplots_adjust(left=0.05, right=0.95, wspace=0.3)
