befecc1cc16479f9258b460906473fb56ece128d
Abhinau Kumar <ab.kumr98@gmail.com>
2020-01-22 15:52:03 -0600

    Parallelizing test folders

--- preprocess_avt_videos.sh
+++ preprocess_avt_videos.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 directory=$1
-avt_directory=$2
+dist_folder=$2
+avt_directory=$3
 
 test_directories="test_1 test_2 test_3 test_4"
 
@@ -10,8 +11,6 @@ do
     ref_filename="${ref_file##*/}"
     ref_filename="${ref_filename%.*}"
 
-    for dist_folder in $test_directories
-    do
         for dist_file in ${directory}/${dist_folder}/segments/${ref_filename}*.mp4
         do
             dist_filename="${dist_file##*/}"
@@ -20,5 +19,4 @@ do
             echo $ref_filename $dist_filename
             ${avt_directory}/gen_avpvs.py --avpvs_folder ${directory}/${dist_folder}/avpvs $dist_file $ref_file
         done
-    done
 done
